28b1e9326b259dc6f1973a4e14fc251345a06135
victorbillaud <victorbillaud@gmail.com>
2023-07-04 16:20:23 +0200

    use getquill to connect database

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,50 +1,24 @@
+package dev.zio.quickstart
+
+import users.InmemoryUserRepo
+import users.PersistentUserRepo
+import users.UserApp
 import zio._
 import zio.http._
-import zio.jdbc._
 
-object HelloWorld extends ZIOAppDefault {
+object MainApp extends ZIOAppDefault:
+  def run: ZIO[Environment with ZIOAppArgs with Scope, Throwable, Any] =
+    val httpApps = UserApp()
+    Server
+      .serve(
+        httpApps.withDefaultErrorResponse
+      )
+      .provide(
+        Server.defaultWithPort(4001),
 
-  val createZIOPoolConfig: ULayer[ZConnectionPoolConfig] =
-    ZLayer.succeed(ZConnectionPoolConfig.default)
+        // An layer responsible for storing the state of the `counterApp`
+        ZLayer.fromZIO(Ref.make(0)),
 
-  val properties: Map[String, String] = Map(
-    "user" -> "postgres",
-    "password" -> "postgres"
-  )
-
-  val connectionPool : ZLayer[ZConnectionPoolConfig, Throwable, ZConnectionPool] =
-    ZConnectionPool.h2mem(
-      database = "testdb",
-      props = properties
-    )
-
-  val create: ZIO[ZConnectionPool, Throwable, Unit] = transaction {
-    execute(
-      sql"CREATE TABLE IF NOT EXISTS ..."
-    )
-  }
-
-  val insertRows: ZIO[ZConnectionPool, Throwable, UpdateResult] = transaction {
-    insert(
-      sql""
-    )
-  }
-
-  val endpoints: App[Any] =
-    Http
-      .collect[Request] {
-        case Method.GET -> !! / "test" => Response.text("Hello World!")
-        case Method.GET -> !! / "init" => ???
-        case Method.GET -> !! / "games" => ???
-        case Method.GET -> !! / "predict" / "game" / gameId => ???
-      }
-      .withDefaultErrorResponse
-
-  val app: ZIO[ZConnectionPool & Server, Throwable, Unit] = for {
-    conn <- create *> insertRows
-    _ <- Server.serve(endpoints)
-  } yield ()
-
-  override def run: ZIO[Any, Throwable, Unit] =
-    app.provide(createZIOPoolConfig >>> connectionPool, Server.default)
-}
+        // To use the persistence layer, provide the `PersistentUserRepo.layer` layer instead
+        PersistentUserRepo.layer
+      )
